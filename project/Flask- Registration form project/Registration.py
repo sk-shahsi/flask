2cb95d7aa596165f082455df7b80355a85978679
sk-shahsi <sk.sashikumar055@gmail.com>
2025-11-25 22:30:16 +0530

    registration form

---
+++ project/Flask- Registration form project/Registration.py
@@ -0,0 +1,23 @@
+# IMPORTING
+from flask import Flask, render_template,request
+#INTRACTION
+web =  Flask(__name__)
+
+#MAPING
+@web.route('/')
+@web.route('/register')
+
+#INPUTS
+def home():
+    return render_template('register.html')
+
+@web.route("/conform" ,methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        city = request.form['city']
+        PhoneNumber = request.form['Phone Number']
+        return render_template('conform.html',name=name,city=city,PhoneNumber=PhoneNumber)
+#MAIN
+if __name__ == '__main__':
+    web.run(debug=True)
